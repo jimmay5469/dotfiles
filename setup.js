a5f1a75e1bcec642c02ecb1568d20d2f7f148d5d
jimmay5469 <jimmay5469@gmail.com>
2014-06-10 18:27:38 -0400

    Make sure to delete any existing dotfiles before creating symlinks.

--- setup.js
+++ setup.js
@@ -2,6 +2,11 @@ var sys = require('sys')
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { /*sys.puts(stdout)*/ }
 
+//remove any existing dotfiles
+exec('rm ~/.bash_profile', puts);
+exec('rm ~/.vimrc', puts);
+exec('rm ~/.gitignore', puts);
+
 //create dotfile symlinks
 exec('ln -s "$PWD/.bash_profile" ~/.bash_profile', puts);
 exec('ln -s "$PWD/.vimrc" ~/.vimrc', puts);
