068ab588dabed289faa4bece87d8460d6b3e9925
jimmay5469 <jimmay5469@gmail.com>
2015-11-06 17:39:48 -0500

    Check for homebrew packages before trying to install

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -8,10 +8,30 @@ if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+
 echo "Installing homebrew packages"
 
-brew install git
-brew install tmux
-brew install macvim
-brew install the_silver_searcher
-brew install editorconfig
+if [ ! "$(brew ls --versions git)" ]; then
+  echo "Installing git"
+  brew install git
+fi
+
+if [ ! "$(brew ls --versions tmux)" ]; then
+  echo "Installing tmux"
+  brew install tmux
+fi
+
+if [ ! "$(brew ls --versions macvim)" ]; then
+  echo "Installing macvim"
+  brew install macvim
+fi
+
+if [ ! "$(brew ls --versions the_silver_searcher)" ]; then
+  echo "Installing the_silver_searcher"
+  brew install the_silver_searcher
+fi
+
+if [ ! "$(brew ls --versions editorconfig)" ]; then
+  echo "Installing editorconfig"
+  brew install editorconfig
+fi
