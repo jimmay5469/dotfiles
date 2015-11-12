84ee0733cca7272e12f1be9d6f2290b70bfc10d5
jimmay5469 <jimmay5469@gmail.com>
2015-11-12 15:46:26 -0500

    The vim script needs the dotfiles installed first

--- install/everything.sh
+++ install/everything.sh
@@ -12,11 +12,12 @@ if [ "$(uname)" == "Darwin" ]; then
 
   source install/oh_my_zsh.sh
   source install/node.sh
-  source install/vim.sh 
   source install/spacemacs.sh 
 
   source install/dotfiles.sh 
   source install/symlinks.sh
+
+  source install/vim.sh
 
 else
 
