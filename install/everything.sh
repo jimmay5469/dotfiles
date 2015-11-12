b01e8c0b986f28a7604d363f1246fdfb76054443
jimmay5469 <jimmay5469@gmail.com>
2015-11-12 00:19:24 -0500

    Move oh-my-zsh install to separate file

--- install/everything.sh
+++ install/everything.sh
@@ -3,13 +3,7 @@
 printf "\n\nCommon installation\n"
 echo "=============================="
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then #oh-my-zsh is not installed
-  if test $(which zsh); then #zsh prerequisite is installed
-    echo "Installing oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  fi
-fi
-
+source install/oh_my_zsh.sh
 source install/nvm.sh
 source install/dotfiles.sh 
 source install/symlinks.sh
---
+++ install/oh_my_zsh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then #oh-my-zsh is not installed
+  if test $(which zsh); then #zsh prerequisite is installed
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  fi
+fi
