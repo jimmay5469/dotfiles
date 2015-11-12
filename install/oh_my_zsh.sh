9705a35660f0381a769f3e517435a9606db96f17
jimmay5469 <jimmay5469@gmail.com>
2015-11-12 08:53:00 -0500

    Clean up oh-my-zsh install

--- install/oh_my_zsh.sh
+++ install/oh_my_zsh.sh
@@ -2,6 +2,8 @@
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then #oh-my-zsh is not installed
   if test $(which zsh); then #zsh prerequisite is installed
+    printf "\nOh My Zsh\n"
+    echo "------------------------------"
     echo "Installing oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   fi
