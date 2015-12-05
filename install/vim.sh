f47dcefcb7e169ad52edf151703b05585078cb81
jimmay5469 <jimmay5469@gmail.com>
2015-12-05 10:33:30 -0500

    Install vim plugins using gvim so we can see progress

--- install/vim.sh
+++ install/vim.sh
@@ -9,7 +9,7 @@ if [ ! -e ~/.vim/autoload/plug.vim ]; then
 fi
 
 echo "Installing vim plugins"
-vim +PlugInstall +qall &> /dev/null
+gvim +PlugInstall +qall #&> /dev/null
 
 #should happen automatically since nvim and vim configs are symlinked
 #echo "Installing neovim plugins"
