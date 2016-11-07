a1f531413aaddf9f61f4b6827836889e18a60526
jimmay5469 <jimmay5469@gmail.com>
2016-11-07 17:06:07 -0500

    Allow nested symlinks

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -5,10 +5,15 @@ printf "\nCreating symlinks\n"
 echo "------------------------------"
 symlinks=$( find $PWD -name '*.symlink' )
 for file in $symlinks ; do
-  symlink="$HOME/$( basename $file ".symlink" )"
-  echo "~${symlink#$HOME}"
+  fileDirectoryName=$( dirname $file )
+  symlinkDirectoryName=${fileDirectoryName/#$PWD/$HOME}
+  symlinkFileName=$( basename $file ".symlink" )
+  symlink="$symlinkDirectoryName/$symlinkFileName"
+
+  echo "~${symlink#$HOME} -> ~${file#$HOME}"
   if [ $symlink ]; then
     rm -rf $symlink
   fi
+  mkdir -p $symlinkDirectoryName
   ln -s $file $symlink
 done
