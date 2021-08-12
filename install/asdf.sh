ea4b8e312d8af2dcb35f93e60a581f7a01d5a1c7
Jimmy Lauzau <jimmay5469@gmail.com>
2021-08-12 10:09:17 -0400

    Remove unused languages from asdf install

--- install/asdf.sh
+++ install/asdf.sh
@@ -11,21 +11,6 @@ if test ! $(which asdf); then
   source $HOME/.shellrc
 fi
 
-if [ ! "$(asdf plugin-list | grep elixir)" ]; then
-  echo "Installing elixir plugin"
-  asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-fi
-
-if [ ! "$(asdf plugin-list | grep elm)" ]; then
-  echo "Installing elm plugin"
-  asdf plugin-add elm https://github.com/vic/asdf-elm.git
-fi
-
-if [ ! "$(asdf plugin-list | grep erlang)" ]; then
-  echo "Installing erlang plugin"
-  asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-fi
-
 if [ ! "$(asdf plugin-list | grep nodejs)" ]; then
   echo "Installing node plugin"
   asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
