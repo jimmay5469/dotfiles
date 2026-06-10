cd2513ba6f34647a0579fc0ef4ed7b4abd405ced
jimmay5469 <jimmay5469@gmail.com>
2026-06-10 16:14:23 -0400

    Node things

--- install/asdf.sh
+++ install/asdf.sh
@@ -6,27 +6,32 @@ echo "------------------------------"
 
 if [ ! "$(asdf plugin list | grep nodejs)" ]; then
   echo "Installing node plugin"
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf plugin add nodejs
 fi
 
 if [ ! "$(asdf plugin list | grep postgres)" ]; then
   echo "Installing postgres plugin"
-  asdf plugin add postgres https://github.com/smashedtoatoms/asdf-postgres.git
+  asdf plugin add postgres
 fi
 
 if [ ! "$(asdf plugin list | grep python)" ]; then
   echo "Installing python plugin"
-  asdf plugin add python https://github.com/tuvistavie/asdf-python.git
+  asdf plugin add python
 fi
 
 if [ ! "$(asdf plugin list | grep ruby)" ]; then
   echo "Installing ruby plugin"
-  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+  asdf plugin add ruby
 fi
 
 if [ ! "$(asdf plugin list | grep yarn)" ]; then
-  echo "Installing ruby yarn"
-  asdf plugin add yarn https://github.com/twuni/asdf-yarn.git
+  echo "Installing yarn plugin"
+  asdf plugin add yarn
+fi
+
+if [ ! "$(asdf plugin list | grep pnpm)" ]; then
+  echo "Installing pnpm plugin"
+  asdf plugin add pnpm
 fi
 
 cd ~/
